2eda42b8da0f44dcb022f887f8e2a272e0ee230f
unknown <clara.camejo@hotmail.com>
2020-05-28 11:46:30 -0300

    error de merge perdieron datos, arreglo de esos datos

--- model/bdModel.php
+++ model/bdModel.php
@@ -6,7 +6,7 @@ class bdModel extends PDO{
 
 
     function __construct(){
-        $this->db = new PDO('mysql:host=localhost;'.'dbname=viajesApp;charset=utf8', 'root', '');
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=viajesapp;charset=utf8', 'root', '');
     }
 
     public function get($id) {
@@ -17,21 +17,21 @@ class bdModel extends PDO{
     }
 
 //funcion GET trae el viaje segun un ID
-    public function getViajes($id){
+/*     public function getViajes($id){
             $sentencia = $this->db->prepare("SELECT * from viajes  WHERE id_viajes = ?");
             $sentencia->execute(array($id));
             $viajes = $sentencia->fetchAll(PDO::FETCH_OBJ);
             return $viajes;
-    }
+    } */
 //funcion GET trae todos los viajes
-    public function getViajes(){
+/*     public function getViajes(){
             $sentencia = $this->db->prepare("SELECT * from viajes");
             $sentencia->execute();
             $viajes = $sentencia->fetchAll(PDO::FETCH_OBJ);
             return $viajes;
-    }
+    } */
 //funcion GET trae un alojamiento segun su ID
-    public function getAlojamientos($id){
+    public function getAlojamientosId($id){
         $sentencia = $this->db->prepare("SELECT * from alojamientos  WHERE id_alojamientos = ?");
         $sentencia->execute(array($id));
         $alojamientos = $sentencia->fetchAll(PDO::FETCH_OBJ);
@@ -47,11 +47,11 @@ class bdModel extends PDO{
 
     
 //funcion POST inserta un viaje
-    public function insertarViajes($origen,$destino,$fecha_inicio,$fecha_fin){
+/*     public function insertarViajes($origen,$destino,$fecha_inicio,$fecha_fin){
 
         $sentencia = $this->db->prepare("INSERT INTO viajes (origen, destino, fecha_inicio, fecha_fin, ) VALUES(?,?,?,?)");
         $sentencia->execute(array($origen,$destino,$fecha_inicio,$fecha_fin));
-    }
+    } */
 //funcion POST inserta un alojamiento
     public function insertarAlojamientos($nombre,$ciudad,$fecha_inicio,$fecha_fin,$descripcion,$contacto,$cod_confirmacion,$checkin,$checkout,$cant_noches,$cant_habitacion,$cant_pasajeros){
 
