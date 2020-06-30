5d0ad4523be077ee32b2109ef1ad71d3d97eea07
Natalia Vytas <natyjazvytas@gmail.com>
2020-06-30 12:19:40 -0300

    add vuelo

--- js/altaVuelo.js
+++ js/altaVuelo.js
@@ -1,6 +1,51 @@
 "use strict"
 document.addEventListener("DOMContentLoaded",cargarPagina);
 
-function cargarPagina(){
-    
-}
+function verificarFormulario(e){
+    e.preventDefault();
+
+    //toma los datos de los campos del formulario
+    let codReserva = document.querySelector("#codigoReserva").value;
+    let compania = document.querySelector("#compania").value;
+    let infoAeronave = document.querySelector("#infoAeronave").value;
+    let nroVuelo = document.querySelector("#nroVuelo").value;
+    let lugarSalida = document.querySelector("#lugarSalida").value;
+    let dataSalida = document.querySelector("#dataSalida").value;
+    let codSalida = document.querySelector("#codSalida").value;
+    let lugarLlegada = document.querySelector("#lugarLlegada").value;
+    let dataLlegada = document.querySelector("#dataLlegada").value;
+    let codLlegada = document.querySelector("#codLlegada").value;
+    let duracion = document.querySelector("#duracion").value;
+    let huella = document.querySelector("#huella").value;
+    let notas = document.querySelector("#notas").value;
+
+    //verifica que los campos nombre, ciudad, fecha inicio y fecha fin esten completos
+    if (
+        codReserva != "" &&
+        compania != "" &&
+        infoAeronave != "" &&
+        nroVuelo != "" &&
+        lugarSalida != "" &&
+        dataSalida != "" &&
+        codSalida != "" &&
+        lugarLlegada != "" &&
+        dataLlegada != "" &&
+        codLlegada != "" &&
+        duracion != "" &&
+        huella != "" &&
+        notas != "" 
+
+    ) {
+      //carga los datos para ser enviados via API y se lo pasa a dicha funcion.
+      let data = {
+        : nombre,
+        destino: ciudad,
+        fecha_inicio: fechaInicio,
+        fecha_fin: fechaFinalizacion,
+        descripcion: InputDescripcion,
+      }
+      cargarViaje(data);
+    }else {
+        alert("Por favor complete los campos obligatorios");
+      }
+  }
